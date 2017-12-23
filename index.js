9cc2411988ad08f43a5bfe3329bfe2bc4db8530a
Kristofer Wallöf <kristofer.wallof@spotonsolutions.se>
2017-12-23 22:44:19 +0100

    cors

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const PORT = process.env.PORT || 5000
-
+const cors = require('cors');
 const app = express();
 
+app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
 app.use('/api', require('./routes/api'));
 
-app  .listen(PORT, () => console.log(`Listening on ${ PORT }`));
+app.listen(PORT, () => console.log(`Listening on ${ PORT }`));
