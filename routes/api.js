19eba08e5ea09905635d8df922514b2ef04f69a6
Kristofer Wallöf <kristofer.wallof@spotonsolutions.se>
2017-12-23 22:31:28 +0100

    Added array and serialize

--- routes/api.js
+++ routes/api.js
@@ -3,15 +3,51 @@ const router = express.Router();
 const googleTrends = require('google-trends-api');
 
 router.get('/trend/:query', (req, res) => {
+    let requestQuery = req.params.query;
+
+    if(requestQuery.includes('+')) {
+        requestQuery = requestQuery.replace('+', " ");
+    }   
+
     googleTrends.interestOverTime({
-        keyword: req.params.query, 
+        keyword: requestQuery, 
         startTime: new Date('2017-01-01'),
         endTime: new Date('2017-12-20'), 
         geo: 'SE'})
         .then((data) => {
             data = JSON.parse(data);
+            if(!data.default.timelineData[0]) {
+                res.send('err');
+            } else {
+                res.send(data.default.timelineData[0].value);
+            }
+        })
+        .catch((err) => {
+            console.log(err);
+        })
+});
 
-            res.send(data.default.timelineData[0].value);
+router.get('/trends', (req, res) => {
+
+    const requestQuery = req.query.array;
+
+    if(requestQuery.length == 0) {
+        res.send(JSON.parse('err'));
+        retun;
+    }
+   
+    googleTrends.interestOverTime({
+        keyword: requestQuery, 
+        startTime: new Date('2017-01-01'),
+        endTime: new Date('2017-12-20'), 
+        geo: 'SE'})
+        .then((data) => {
+            data = JSON.parse(data);
+            if(!data.default.timelineData[0]) {
+                res.send('err');
+            } else {
+                res.send(data.default.timelineData[0].value);
+            }
         })
         .catch((err) => {
             console.log(err);
