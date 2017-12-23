f0e8bd76b090b811820aa6960aa0ded4f0b5266a
Kristofer Wallöf <kristofer.wallof@spotonsolutions.se>
2017-12-23 23:13:52 +0100

    whiteList cors

--- index.js
+++ index.js
@@ -5,6 +5,17 @@ const cors = require('cors');
 const app = express();
 
 app.use(cors());
+var whitelist = ['http://example1.com', 'http://example2.com']
+var corsOptions = {
+  origin: function (origin, callback) {
+    if (whitelist.indexOf(origin) !== -1) {
+      callback(null, true)
+    } else {
+      callback(new Error('Not allowed by CORS'))
+    }
+  }
+}
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
